4a03704f914868da8deb87413300b3196ea06238
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-04-02 01:45:12 +0100

    commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const inputfield = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
+
+addButtonEl.addEventListener("click", function () {
+let inputValue = inputfield.value;
+});
