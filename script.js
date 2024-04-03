6f4d51cb84e256437e98614080fc1d790a25eccf
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-04-03 23:17:31 +0100

    script edit

--- script.js
+++ script.js
@@ -1,33 +1,68 @@
-import {initializeApp} from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js'
-import {getDatabase, ref, push} from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js'
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSetttings = {
-    databaseURL: 'https://realtime-database-852c7-default-rtdb.firebaseio.com/'
-}
+  databaseURL: "https://realtime-database-852c7-default-rtdb.firebaseio.com/",
+};
 
 const app = initializeApp(appSetttings);
 const database = getDatabase(app);
-const shoppingInDB = ref(database, 'Shopping List')
+const shoppingInDB = ref(database, "Shopping List");
+
+onValue(shoppingInDB, function (snapshot) {
+//   let ItemsArray = Object.values(snapshot.val());
+//   let ItemsArrayId = Object.keys(snapshot.val());
 
 
+  let ItemsArray = Object.entries(snapshot.val());
 
+
+  clearShoppingList();
+  emptyField();
+  for (let i = 0; i < ItemsArray.length; i++) {
+    let currentItems = ItemsArray[i];
+
+let currentItemID = currentItems[0];
+let currentItemsValue = currentItems[1]
+
+   let itemValue   = newItems(  ItemsArray );
+
+  }
+});
 
 const inputfield = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-button");
-const shoppingList = document.getElementById('shopping-list');
+const shoppingList = document.getElementById("shopping-list");
 
 addButtonEl.addEventListener("click", function () {
-let inputValue = inputfield.value;
-push(shoppingInDB, inputValue)
+  let inputValue = inputfield.value;
+  push(shoppingInDB, inputValue);
 
-emptyField();
-newItems(inputValue);
+  emptyField();
 });
 
-function emptyField(){
-    inputfield.value = ''
+function clearShoppingList() {
+  shoppingList.innerHTML = "";
 }
 
-function newItems(itemValue){
-    shoppingList.innerHTML = `<li>${itemValue}</>`
+function emptyField() {
+  inputfield.value = "";
+}
+
+function newItems(Items) {
+//   shoppingList.innerHTML += `<li>${itemValue}</>`;
+let ItemID = Items[0];
+let ItemsValue = Items[1];
+
+    let newEl = document.createElement('li');
+
+    newEl.textContent = ItemID
+
+    shoppingList.appendChild(newEl);
+
 }
